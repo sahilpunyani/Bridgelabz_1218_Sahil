e7b63b83e483792683d128912012da184a69a5b1
Sahil Punyani <sahilpunyani0909@gmail.com>
2024-12-05 15:24:49 +0530

    logical operator

---
+++ operator.js
@@ -0,0 +1,43 @@
+//comparison operator
+//Equal
+let a = 5;
+let b = '5';
+console.log(a == b);
+
+//Strict Equal
+let c = 5;
+let d = '5';
+console.log(c === d);
+
+//Not Equal
+let e = 5;
+let f = '5';
+console.log(e != f);
+
+//Strict Not Equal
+let g = 5;
+let h = '5';
+console.log(g !== h);
+
+//Greater than
+let i = 10;
+let j = 5;
+console.log(i > j);  
+
+//Less than
+let k = 10;
+let l = 5;
+console.log(k < l);  
+
+//Greater than or Equal
+let m = 10;
+let n = 10;
+console.log(m >= n);
+
+//Less than or Equal
+let o = 10;
+let p = 15;
+console.log(0 <= p);
+
+
+
