40d044736f4689d81c285683b063203fc330000e
Sahil Punyani <sahilpunyani0909@gmail.com>
2024-12-12 15:41:16 +0530

    31

---
+++ 31.js
@@ -0,0 +1,9 @@
+//Create a function that will return the number of words in a text
+let str="my name is sahil";
+let arr=str.split(' ');
+let count=0;
+for(let i=0; i<arr.length; i++){
+console.log(arr[i]);
+count++;
+}
+console.log(count);
